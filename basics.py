376dbef97493a22fe12c2a49087381e0aec79c5b
nancy yadav <nancyyadav2988@gmail.com>
2026-01-20 22:25:28 +0530

    Add files via upload
    This repository contains my Python basics practice programs.
    I created this repo to understand and practice core Python concepts step-by-step.
    
     Concepts Covered
    
    Variables and data types
    
    Checking data type using type()
    
    Taking input from the user
    
    Type casting (int(), str())
    
    Basic arithmetic operations
    
    Printing output in different formats
    
    Simple beginner-level example

---
+++ basics.py
@@ -0,0 +1,27 @@
+# basics , checking datatype 
+a = 9
+print( type( a))
+
+# taking input fron user 
+age = input ( " enter your age ")
+print (age ) 
+
+# type casting 
+age = int ( input ( " enter your age "))
+print ( age + 5)
+
+a = int ( input ( "enter the value of a "))
+b = int ( input ( " enter the value of b "))
+print ( a + b)
+
+# output 
+name = "nancy" 
+age = 18 
+print ( name )
+print ( age )
+print ( " there is a girl name" , name , " her age is ", age )
+# type casting using string 
+a=2 
+b=3 
+msg = "sum is " + str( a+ b)
+print( msg)
