ada8e923ffa5e891d1e72a64b9ca1b56d6eedc36
nancy yadav <nancyyadav2988@gmail.com>
2026-01-21 20:57:50 +0530

    Add files via upload
    Beginner-friendly Python programs covering assignment operations, salary update, multiplication and division using assingment using user input.

---
+++ assignment_operator.py
@@ -0,0 +1,18 @@
+#increase no by 10 
+
+num=int(input("enter any number"))
+num+=10
+print("after addtion ", num)
+
+#wap of salary update( bonus + tax)
+salary=int(input("enter the salary"))
+salary+=2000
+salary-=500
+print("final salary is ",salary) 
+
+# multiplication and division assignment op
+num=int(input("enter the number"))
+num*=2
+print("after multiplication",num)
+num /= 2
+print("after division",num)
